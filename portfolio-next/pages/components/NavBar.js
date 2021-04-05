95d11ee73cbb2a187756cdcacdfb758e39881ce3
David Carrillo Jr <djcarrillo6@yahoo.com>
2021-04-04 19:38:56 -0700

    Landing page design.

---
+++ portfolio-next/pages/components/NavBar.js
@@ -0,0 +1,22 @@
+ import styles from '../../styles/NavBar.module.css'
+
+ const NavBar = () => {
+
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <h1 className="|"><a href="/">DJ's Portfolio</a></h1>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
--- portfolio-next/pages/index.js
+++ portfolio-next/pages/index.js
@@ -1,8 +1,88 @@
+import React, { useEffect } from "react";
+
 import Head from 'next/head'
+import NavBar from './components/NavBar';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+ 
+
   return (
-    <div>Home Component</div> 
+    <div className={styles.container}>
+        < NavBar />
+
+        <div className={styles.animatedGradientContainer}>
+          <p className={styles.text} >Hi I'm DJ. I design & build web and mobile apps.
+          </p>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+        </div>
+
+        <div className={styles.animatedGradientContainer}>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+
+        <p className={styles.text} >Software Engineer. Mixologist. Micro-Economist.</p>
+        </div>
+
+        <div className={styles.animatedGradientContainer}>
+
+        <p className={styles.text} >"To improve is to change; to be perfect is to change often.    --W.Churchill 1944"</p>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+        </div>
+
+
+        <div className={styles.animatedGradientContainer}>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+        </div>
+
+        <p className={styles.projectsHeader} >Projects</p>
+        <div className={styles.animatedGradientContainer}>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+          <div className={styles.circle} ></div>
+          <div className={styles.circle2} ></div>
+        </div>
+    </div>
   )
 }
